cde47a203b6d08be8cedc8e25d71d025856a98cf
Tobias Laving <tobias@trine.com>
2022-11-07 12:05:30 +0100

    Support comma seperated list of vmss when updating
    In our environment we have several vmss using the same managed image. This small addition
    will allow the pipeline to update them all

--- scripts/update-vmss.ps1
+++ scripts/update-vmss.ps1
@@ -4,12 +4,15 @@ param(
     [String] [Parameter (Mandatory=$true)] $TenantId,
     [String] [Parameter (Mandatory=$true)] $SubscriptionId,
     [String] [Parameter (Mandatory=$true)] $ResourceGroup,
-    [String] [Parameter (Mandatory=$true)] $VmssName,
+    [String] [Parameter (Mandatory=$true)] $VmssNames,
     [String] [Parameter (Mandatory=$true)] $ManagedImageId
 )
 
 az login --service-principal --username $ClientId --password $ClientSecret --tenant $TenantId | Out-Null
 az account set -s $SubscriptionId
 
-az vmss update --resource-group $ResourceGroup --name $VmssName --set virtualMachineProfile.storageProfile.imageReference.id=$ManagedImageId
-Write-Host "Updated Virtual Machine Scale Set with new Azure Image: $ManagedImageId"
+$VmssNames.Split(",") | ForEach {
+  $VmssName = $_
+  az vmss update --resource-group $ResourceGroup --name $VmssName --set virtualMachineProfile.storageProfile.imageReference.id=$ManagedImageId
+  Write-Host "Updated Virtual Machine Scale Set ManagedImageId: $VmssName - $ManagedImageId"
+}
