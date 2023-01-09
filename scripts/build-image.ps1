fe797244b3206ee70264d07545a3654a73d71a1d
erik-de-bont <erik@debont.net>
2023-01-09 13:54:26 +0100

    corrected packer to build image in gallery

--- scripts/build-image.ps1
+++ scripts/build-image.ps1
@@ -42,12 +42,10 @@ Write-Host "Show Packer Version"
 packer --version
 
 Write-Host "Build $Image VM"
-packer build    -var "capture_name_prefix=$ResourcesNamePrefix" `
-                -var "client_id=$ClientId" `
+packer build    -var "client_id=$ClientId" `
                 -var "client_secret=$ClientSecret" `
                 -var "install_password=$InstallPassword" `
                 -var "location=$Location" `
-                -var "resource_group=$ResourceGroup" `
                 -var "subscription_id=$SubscriptionId" `
                 -var "temp_resource_group_name=$TempResourceGroupName" `
                 -var "tenant_id=$TenantId" `
