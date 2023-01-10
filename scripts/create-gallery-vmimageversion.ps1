0a1238d5d62c1176420e6803a7390cf8e6cc8cb8
erik-de-bont <erik@debont.net>
2023-01-10 09:34:33 +0100

    rollback to managed image

--- scripts/create-gallery-vmimageversion.ps1
+++ scripts/create-gallery-vmimageversion.ps1
@@ -6,35 +6,31 @@ param(
     [String] [Parameter (Mandatory=$true)] $ResourcesNamePrefix,
     [String] [Parameter (Mandatory=$true)] $Location,
     [String] [Parameter (Mandatory=$true)] $ResourceGroup,
-    [String] [Parameter (Mandatory=$true)] $StorageAccount,
-    [String] [Parameter (Mandatory=$true)] $ImageType,
-    [String] [Parameter (Mandatory=$true)] $OsVhdUri,
-    [String] [Parameter (Mandatory=$true)] $OsType,
     [String] [Parameter (Mandatory=$true)] $GalleryName,
-    [String] [Parameter (Mandatory=$true)] $GalleryResourceGroup
+    [String] [Parameter (Mandatory=$true)] $GalleryResourceGroup,
+    [String] [Parameter (Mandatory=$true)] $ImageType,
+    [String] [Parameter (Mandatory=$true)] $ManagedImageId
 )
 
 az login --service-principal --username $ClientId --password $ClientSecret --tenant $TenantId | Out-Null
 az account set -s $SubscriptionId
 
 $date = Get-Date
-$GalleryImageVersion = $date.ToString("yyyy.MM.dd")
+$ImageVersion = $date.ToString("yyyy.MM.dd")
+
 $GalleryVmImageDefinition = "$ImageType-agentpool-full"
+$VmImageVersion = az gallery image version  create -g $GalleryResourceGroup  --gallery-name $GalleryName --$GalleryVmImageDefinition -name $ImageVersion -manage-image $ManagedImageId
 
-$StorageAccountObject = Get-AzStorageAccount -ResourceGroupName $ResourceGroup -Name $StorageAccount 
-$StorageAccountId = $StorageAccountObject.Id
+Write-Host "##vso[task.setvariable variable=VmImageVersion;isOutput=true;]$VmImageVersion"
 
-$VmImageVersion = az sig image-version create --resource-group $GalleryResourceGroup --gallery-name $GalleryName --gallery-image-definition $GalleryVmImageDefinition  --gallery-image-version $GalleryImageVersion --os-vhd-uri $OsVhdUri -os-vhd-storage-account $StorageAccountId
+Write-Host "Update Gallery Image: $GalleryImageDefinition"
+Write-Host "Created VM Image Version: $VMImageversion"
 
-Write-Host "##vso[task.setvariable variable=$VmImageVersion;isOutput=true;]$VmImageVersion"
 
-Write-Host "Created in $Gallery new $GalleryVmImageDefinition Version: $GalleryImageVersion"
 
-$storageContainer = "Microsoft.Compute/Images/images/$ResourcesNamePrefix"
-$existingBlobs = az storage blob list -c system --prefix $storageContainer --account-name $StorageAccount --auth-mode login --query "[].name" | Out-String
-$existingBlobs = ConvertFrom-Json $existingBlobs
-
-foreach ($blob in $existingBlobs) {
-    Write-Host "Found a match, deleting Packer generated file $blob"
-    az storage blob delete -c system -n $blob --account-name $StorageAccount --delete-snapshots include --auth-mode login
-}
+#az gallery image version create \
+#  --resource-group myResourceGroup \
+#  --gallery-name myGallery \
+#  --gallery-image-definition myImageDefinition \
+#  --name myImageVersion \
+#  --managed-image '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/images/{imageName}'
