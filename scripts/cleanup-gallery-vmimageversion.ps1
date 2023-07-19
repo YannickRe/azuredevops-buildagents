f0415552d6117e5ccc5987f5a1a2a1ae55d0ab74
Erik de Bont <71345042+erik-de-bont@users.noreply.github.com>
2023-07-19 13:18:34 +0200

    Corrected variable GalleryResourcGroup

--- scripts/cleanup-gallery-vmimageversion.ps1
+++ scripts/cleanup-gallery-vmimageversion.ps1
@@ -25,7 +25,6 @@ import-module Az.Compute
 $securePassword = ConvertTo-SecureString -String $ClientSecret -AsPlainText -Force
 $credentials = New-Object -TypeName PSCredential -ArgumentList $ClientId, $securePassword
 Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential $credentials -Subscription $SubscriptionId
-Set-AzContext -SubscriptionId $SubscriptionId
 
 # Set the image threshold
 # Threshhold value 3 will remove the oldest vm images if there 3 or more images available in the gallery. This value can be changed to your needs.
@@ -33,7 +32,7 @@ $imageCountThreshold = 3
 
 # Get the gallery
 try {
-    $gallery = Get-AzGallery -Name $GalleryName -ResourceGroupName $ResourceGroup 
+    $gallery = Get-AzGallery -Name $GalleryName -ResourceGroupName $GalleryResourceGroup 
     }
 catch  {
     Write-Host "##vso[task.logissue type=error;] Gallery '$GalleryName' not found"
@@ -48,7 +47,7 @@ if ($gallery) {
     foreach ($imageDefinition in $imageDefinitions) {
         # Get the image definition
         try {
-        $imageDef = Get-AzGalleryImageDefinition  -ResourceGroupName $ResourceGroup -GalleryName $gallery.Name  -GalleryImageDefinitionName $imageDefinition
+        $imageDef = Get-AzGalleryImageDefinition  -ResourceGroupName $GalleryResourceGroup -GalleryName $gallery.Name  -GalleryImageDefinitionName $imageDefinition
             }
         catch {
             Write-Host "##vso[task.logissue type=error;] Imagedefinition '$imageDefinition' not found"
@@ -58,7 +57,7 @@ if ($gallery) {
                 
         if ($imageDef) {
             # Get all images of the image definition
-            $images = Get-AzGalleryImageVersion -ResourceGroupName $ResourceGroup -GalleryImageDefinitionName $imageDef.Name  -GalleryName $gallery.Name
+            $images = Get-AzGalleryImageVersion -ResourceGroupName $GalleryResourceGroup -GalleryImageDefinitionName $imageDef.Name  -GalleryName $gallery.Name
                
             if ($images.Count -ge $imageCountThreshold) {
                 # Sort the images by creation timestamp in ascending order
@@ -68,7 +67,7 @@ if ($gallery) {
                 $imageToRemove = $sortedImages[0]
 
                 Write-Host "##[section]Removing the oldest image version for image definition '$imageDefinition': $($imageToRemove.Name)"
-                Remove-AzGalleryImageVersion -ResourceGroupName $ResourceGroup -GalleryName $gallery.Name -GalleryImageDefinitionName $imageDefinition -Name $imageToRemove.Name -Force -AsJob 
+                Remove-AzGalleryImageVersion -ResourceGroupName $GalleryResourceGroup -GalleryName $gallery.Name -GalleryImageDefinitionName $imageDefinition -Name $imageToRemove.Name -Force -AsJob 
             } else {
                 Write-Host "##[section]The number of images for image definition '$imageDefinition' is less than $imageCountThreshold"
             }
