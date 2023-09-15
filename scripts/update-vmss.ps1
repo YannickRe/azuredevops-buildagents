53cdc26c71f5bf01f96d0fb0aa85cb1c4a1eae19
Erik de Bont <71345042+erik-de-bont@users.noreply.github.com>
2023-09-15 08:13:13 +0200

    added managedImageId retrieval

--- scripts/update-vmss.ps1
+++ scripts/update-vmss.ps1
@@ -4,14 +4,17 @@ param(
     [String] [Parameter (Mandatory=$true)] $TenantId,
     [String] [Parameter (Mandatory=$true)] $SubscriptionId,
     [String] [Parameter (Mandatory=$true)] $ResourceGroup,
-    [String] [Parameter (Mandatory=$true)] $VmssNames,
-    [String] [Parameter (Mandatory=$true)] $ManagedImageId
+    [String] [Parameter (Mandatory=$true)] $VmssNames
 )
 
 az login --service-principal --username $ClientId --password $ClientSecret --tenant $TenantId | Out-Null
 az account set -s $SubscriptionId
 
-$VmssNames.Split(",") | ForEach {
+$imageName = "$ImageType-$ResourcesNamePrefix"
+$managedImageId=$(az image list --resource-group $ResourceGroup --query "[?name=='$imageName'].id" --output tsv)
+Write-Host "Retrieve generated managedImageId: $managedImageId"
+
+$VmssNames.Split(",") | ForEach-Object {
   $VmssName = $_
   az vmss update --resource-group $ResourceGroup --name $VmssName --set virtualMachineProfile.storageProfile.imageReference.id=$ManagedImageId
   Write-Host "Updated Virtual Machine Scale Set ManagedImageId: $VmssName - $ManagedImageId"
