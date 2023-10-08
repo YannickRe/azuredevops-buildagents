94b94dcaf811418bfb2348aee5b69fd610654243
Yannick Reekmans <YannickRe@users.noreply.github.com>
2023-10-09 01:08:45 +0200

    Update build-image.ps1

--- scripts/build-image.ps1
+++ scripts/build-image.ps1
@@ -5,7 +5,6 @@ param(
     [String] [Parameter (Mandatory=$true)] $ResourcesNamePrefix,
     [String] [Parameter (Mandatory=$true)] $Location,
     [String] [Parameter (Mandatory=$true)] $ResourceGroup,
-    [String] [Parameter (Mandatory=$true)] $StorageAccount,
     [String] [Parameter (Mandatory=$true)] $SubscriptionId,
     [String] [Parameter (Mandatory=$true)] $TenantId,
     [String] [Parameter (Mandatory=$true)] $CaptureNamePrefix,
