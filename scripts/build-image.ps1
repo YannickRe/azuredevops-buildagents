b4eb72025262d708c21cc5e7336e1480aaec5b08
mvpwasilewskim <42752194+mvpwasilewskim@users.noreply.github.com>
2023-12-18 11:35:45 -0500

    Update build-image.ps1

--- scripts/build-image.ps1
+++ scripts/build-image.ps1
@@ -2,15 +2,11 @@ param(
     [String] [Parameter (Mandatory=$true)] $TemplatePath,
     [String] [Parameter (Mandatory=$true)] $ClientId,
     [String] [Parameter (Mandatory=$true)] $ClientSecret,
-    [String] [Parameter (Mandatory=$true)] $ResourcesNamePrefix,
     [String] [Parameter (Mandatory=$true)] $Location,
     [String] [Parameter (Mandatory=$true)] $ResourceGroup,
     [String] [Parameter (Mandatory=$true)] $SubscriptionId,
     [String] [Parameter (Mandatory=$true)] $TenantId,
-    [String] [Parameter (Mandatory=$true)] $CaptureNamePrefix,
-    [String] [Parameter (Mandatory=$true)] $GalleryResourceGroup,
-    [String] [Parameter (Mandatory=$true)] $GalleryName,
-    [String] [Parameter (Mandatory=$true)] $ImageType, 
+    [String] [Parameter (Mandatory=$true)] $ImageName, 
     [String] [Parameter (Mandatory=$false)] $VirtualNetworkName,
     [String] [Parameter (Mandatory=$false)] $VirtualNetworkRG,
     [String] [Parameter (Mandatory=$false)] $VirtualNetworkSubnet
@@ -53,7 +49,7 @@ packer build    -var "client_id=$ClientId" `
                 -var "virtual_network_resource_group_name=$VirtualNetworkRG" `
                 -var "virtual_network_subnet_name=$VirtualNetworkSubnet" `
                 -var "run_validation_diskspace=$env:RUN_VALIDATION_FLAG" `
-                -var "managed_image_name=$ImageType-$ResourcesNamePrefix" `
+                -var "managed_image_name=$ImageName" `
                 -var "managed_image_resource_group_name=$ResourceGroup" `
                 -color=false `
                 $TemplatePath `
